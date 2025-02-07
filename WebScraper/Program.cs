f14a1b4e23f93609c965b463906eaeddf6ac4ebd
Margarita Maneva <37753481+mmaneva@users.noreply.github.com>
2025-02-07 10:48:36 +0100

    Web scraping using HtmlAgilityPack

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -1,7 +1,31 @@
-﻿public class WebScraper
+﻿using HtmlAgilityPack;
+public class WebScraper
 {
     public static async Task Main(string[] args)
     {
+        var url = "https://en.wikipedia.org/wiki/OpenAI";
+        var httpClient = new HttpClient();
+        var html = await httpClient.GetStringAsync(url);
 
+       
+
+        var htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml(html);
+
+        var divs = htmlDocument.DocumentNode.Descendants("div")
+            .Where(node => node.GetAttributeValue("class", "")
+            .Contains("mw-parser-output")).ToList();
+
+        foreach ( var div in divs)
+        {
+            var paragraphs = div.Descendants("p").Where(p => !string.IsNullOrWhiteSpace(p.InnerText)).ToList();
+            if(paragraphs.Count > 0)
+            {
+                Console.WriteLine(paragraphs[0].InnerText.Trim());
+                break;
+            }
+                
+            //Console.WriteLine(div.InnerText.Trim());
+        }
     }
 }
