0b9b18eb06ad327bf099e4035125e68e0b2a039a
Margarita Maneva <37753481+mmaneva@users.noreply.github.com>
2025-02-09 18:04:02 +0100

    Improved Web Scraper code with CSV storage

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -1,31 +1,73 @@
-﻿using HtmlAgilityPack;
+﻿using CsvHelper;
+using HtmlAgilityPack;
+using System.Globalization;
+
+public class Product 
+{
+    public string Name { get; set; }
+    public string Price { get; set; }
+}
+
 public class WebScraper
 {
     public static async Task Main(string[] args)
     {
-        var url = "https://en.wikipedia.org/wiki/OpenAI";
+        var url = "";
         var httpClient = new HttpClient();
         var html = await httpClient.GetStringAsync(url);
-
-       
 
         var htmlDocument = new HtmlDocument();
         htmlDocument.LoadHtml(html);
 
-        var divs = htmlDocument.DocumentNode.Descendants("div")
-            .Where(node => node.GetAttributeValue("class", "")
-            .Contains("mw-parser-output")).ToList();
+        var products = new List<Product>();
 
-        foreach ( var div in divs)
+        var productNodes = htmlDocument.DocumentNode.SelectNodes("//div[contains(@class, 'product-card')]");
+
+        if (productNodes != null)
         {
-            var paragraphs = div.Descendants("p").Where(p => !string.IsNullOrWhiteSpace(p.InnerText)).ToList();
-            if(paragraphs.Count > 0)
+            foreach (var productNode in productNodes)
             {
-                Console.WriteLine(paragraphs[0].InnerText.Trim());
-                break;
+                try
+                {
+                    var nameNode = productNode.SelectSingleNode("");
+                    var priceNode = productNode.SelectSingleNode("");
+
+                    if (nameNode != null && priceNode != null)
+                    {
+                        var product = new Product
+                        {
+                            Name = nameNode.InnerText.Trim(),
+                            Price = priceNode.InnerText.Trim()
+                        };
+                        products.Add(product);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
-                
-            //Console.WriteLine(div.InnerText.Trim());
         }
+        else
+        {
+            Console.WriteLine("No products found on the page.");
+        }
+
+        
     }
+
+    public static void SaveToCsv(List<Product> products)
+    {
+        var filePath = "products.csv";
+
+        using (var writer = new StreamWriter(filePath))
+        using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+        {
+            csv.WriteRecords(products);
+        }
+
+        Console.WriteLine($"Data saved to {filePath}");
+    }
+
+        
 }
